39ce2a7e57ccf9ad932f3913853076f02b2843e4
Daulton <jenkins.daulton@gmail.com>
2026-02-20 23:03:07 -0400

    fix(v0.8.3.0): prepare for play store hosting

---
+++ android/app/src/main/java/dev/gettilted/app/DelegationService.java
@@ -0,0 +1,14 @@
+package dev.gettilted.app;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
